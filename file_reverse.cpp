b42e22dadda77c3986596da46ac72d2c2bd866a8
Vincent Titterton <vince8nt@gmail.com>
2025-09-16 19:50:27 -0700

    clear failbits to work with super long lines

--- file_reverse.cpp
+++ file_reverse.cpp
@@ -4,32 +4,41 @@
 #include <array>
 #include <algorithm>
 
-// fixed size buffer stored on the stack
-// better performance than std::string
+// Fixed size buffer stored on the stack
+// Better performance than std::string
 std::array<char, 4096> buffer;
 
-// read until newline or eof
-// resulting buffer might only contain ending portion of the line (for lines longer than the buffer)
-// returns the file position cooresponding to the start of the buffer
-size_t read_until_newline(std::ifstream& in_file) {
+// Clear in_file's error flags, then read until newline, eof, or full buffer
+// Return the file position cooresponding to the start of the buffer
+inline size_t getline_pos(std::ifstream& in_file) {
+    in_file.clear();
     size_t buffer_start = in_file.tellg();
     in_file.getline(buffer.data(), buffer.size());
+    return buffer_start;
+}
+
+// Read until newline or eof
+// Resulting buffer might only contain ending portion of the line (for lines longer than the buffer)
+// Returns the file position cooresponding to the start of the buffer
+size_t read_until_newline(std::ifstream& in_file) {
+    size_t buffer_start = getline_pos(in_file);
     while (true) {
         switch (in_file.rdstate()) {
             case std::ios::badbit:
                 std::cerr << "Error: failed to read from input file: " << std::endl;
                 exit(1);
             case std::ios::failbit:
-                buffer_start = in_file.tellg();
-                in_file.getline(buffer.data(), buffer.size());
+                buffer_start = getline_pos(in_file);
                 break;
+            case std::ios::eofbit:
+                return buffer_start;
             default:
                 return buffer_start;
         }
     }
 }
 
-// reverse the (portion of) buffer and write to output file
+// Reverse the (portion of) buffer and write to output file
 void reverse_write_buffer(std::ofstream& out_file, const size_t size) {
     std::reverse(buffer.begin(), buffer.begin() + size);
     out_file.write(buffer.data(), size);
@@ -39,31 +48,33 @@ void reverse_write_buffer(std::ofstream& out_file, const size_t size) {
     }
 }
 
-// writes the reverse line to the output file
+// Writes the reverse line to the output file
 // in_file: the input file stream (positioned after reading the line)
 // out_file: the output file stream
 // line_start: the file position cooresponding to the start of the line
 // buffer_start: the file position cooresponding to the start of the buffer
-// returns true if eof is reached
+// Returns true if eof is reached
 bool write_reverse_line(std::ifstream& in_file, std::ofstream& out_file) {
     const size_t line_start = in_file.tellg();
     size_t buffer_start = read_until_newline(in_file);
-    const size_t next_line_start = in_file.tellg();
+    const size_t next_line_start = in_file.tellg(); // could be garbage if eof is reached
     const bool eof = in_file.eof();
     
-    // empty line at the end of the file
+    // Empty line at the end of the file
     if (buffer_start == next_line_start) {
         return true;
     }
 
-    // if eof is not reached, gcount() will include the newline (which wasn't read into the buffer)
-    // this works for both Unix/Linux (newline is \n) and Windows (newline is \r\n)
+    // If eof is not reached, gcount() will count the newline (which wasn't read into the buffer)
+    // This works for both Unix/Linux (newline is \n) and Windows (newline is \r\n)
+    //// since \r\n is transormed into \n prior to gcount()'s calculation.
     reverse_write_buffer(out_file, in_file.gcount() - (eof ? 0 : 1));
 
-    // seek backwards to read from line longer than the buffer
+    // Seek backwards to read from line longer than the buffer
     while (buffer_start > line_start) {
         size_t read_size = std::min(buffer_start - line_start, buffer.size());
         buffer_start -= read_size;
+        in_file.clear();
         in_file.seekg(buffer_start);
         in_file.read(buffer.data(), read_size);
         if (in_file.bad()) {
@@ -73,7 +84,7 @@ bool write_reverse_line(std::ifstream& in_file, std::ofstream& out_file) {
         reverse_write_buffer(out_file, read_size);
     }
     
-    // write newline and seek to initial in_file position
+    // Write newline and seek to initial in_file position
     if (!eof) {
         if (in_file.tellg() != next_line_start) {
             in_file.seekg(next_line_start);
@@ -105,8 +116,8 @@ int main(int argc, char** argv) {
         exit(1);
     }
 
-    // read file line by line:
-    //// for short lines, fstream's internal buffering will make this efficient
-    //// for lines longer than the buffer, we repeatidly read a 4096 byte chunk until we find the newline
+    // Read file line by line:
+    //// For short lines, fstream's internal buffering will make this efficient
+    //// For lines longer than the buffer, we read a 4096 byte chunk until we find the newline
     while (write_reverse_line(in_file, out_file) == false);
 }
